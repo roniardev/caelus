d95ae45b3a454b89d054524b5ef1f3652c25ad9f
infrasync <roon.ardiyanto@gmail.com>
2023-10-03 23:14:43 +0700

    feat(app) : init template

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+});
