3add56ce0957e0eb2ba853ae790210784b0143a0
infrasync <roon.ardiyanto@gmail.com>
2024-05-17 11:26:31 +0700

    :bug: fix(app): remove shadow add border top

--- styled-system/patterns/visually-hidden.mjs
+++ styled-system/patterns/visually-hidden.mjs
@@ -2,16 +2,14 @@ import { mapObject } from '../helpers.mjs';
 import { css } from '../css/index.mjs';
 
 const visuallyHiddenConfig = {
-  transform(props) {
-    return {
-      srOnly: true,
-      ...props,
-    };
-  },
-};
+transform(props) {
+  return {
+    srOnly: true,
+    ...props
+  };
+}}
 
-export const getVisuallyHiddenStyle = (styles = {}) =>
-  visuallyHiddenConfig.transform(styles, { map: mapObject });
+export const getVisuallyHiddenStyle = (styles = {}) => visuallyHiddenConfig.transform(styles, { map: mapObject })
 
-export const visuallyHidden = (styles) => css(getVisuallyHiddenStyle(styles));
-visuallyHidden.raw = getVisuallyHiddenStyle;
+export const visuallyHidden = (styles) => css(getVisuallyHiddenStyle(styles))
+visuallyHidden.raw = getVisuallyHiddenStyle
